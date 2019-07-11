661b7af21292912045f366c87656b75f85970802
Jose Cerqueira <jose@origensmedia.com>
2019-07-11 10:09:39 +0100

    Heading the header component to app with links to pages

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 
 import './App.scss';
+import Header from './components/header/header';
 import HomePage from './pages/homepage/HomePage';
 import ShopPage from './pages/shop/ShopPage';
 
 const App = props => {
 	return (
 		<div>
+			<Header />
 			<Switch>
 				<Route exact path="/" component={HomePage} />
 				<Route exact path="/shop" component={ShopPage} />
